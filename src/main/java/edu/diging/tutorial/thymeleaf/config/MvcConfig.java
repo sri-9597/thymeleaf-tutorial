0c69e2ea8b1b2ade1a00ce2e049b26b99519bc8a
Sreechandra Allala <srichandraallala@gmail.com>
2023-01-20 17:06:09 -0700

    Initial Commit

---
+++ src/main/java/edu/diging/tutorial/thymeleaf/config/MvcConfig.java
@@ -0,0 +1,9 @@
+package edu.diging.tutorial.thymeleaf.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("edu.asu.diging.simpleusers.web")
+public class MvcConfig {
+
+}
---
+++ src/main/java/edu/diging/tutorial/thymeleaf/config/SimpleUsersConfig.java
@@ -0,0 +1,15 @@
+package edu.diging.tutorial.thymeleaf.config;
+import org.springframework.context.annotation.Configuration;
+
+import edu.asu.diging.simpleusers.core.config.SimpleUsers;
+import edu.asu.diging.simpleusers.core.config.SimpleUsersConfiguration;
+
+
+@Configuration
+public class SimpleUsersConfig implements SimpleUsersConfiguration {
+
+    @Override
+    public void configure(SimpleUsers simpleUsers) {
+        
+    }
+}
